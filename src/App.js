c86e5a64fcfbc8fdcacaf995bcee82f2b23abe5f
Daniel Ohuruogu <daniel.ohuruogu@hotmail.co.uk>
2021-04-21 19:31:09 +0100

    add state change to change text through a button

--- src/App.js
+++ src/App.js
@@ -1,67 +1,39 @@
 import React from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
-import { useState, useEffect } from 'react'
-import TravellersTrollies from './Components/General/AppLayout/AppLayoutMain';
+import { useState } from 'react'
+// import TravellersTrollies from './Components/General/AppLayout/AppLayoutMain';
 
 import ContentContainer from './Components/General/ContentContainer/ContentContainerMain'
 
 function App() {
-
 
   // STATES TO MANAGE THE SCREEN DISPLAYS
 
   //for the user
   const [userLoggedIn, setUserLoggedIn] = useState(false)
   // if the user is a setter or a completer
-  const [isSetter, setSetter] = useState(false)
+  const [isSetter, setSetter] = useState(false);
 
-
-
-  // will need to create a function that responds to the user's input
-  // how to do that...?
 
   /* if (isSetter == false) {
-    change the state of the modules below
-    will 
-  }
-
-  */
-
-  // const getPost = async () => {
-    
-  //   const res = await fetch("https://jsonplaceholder.typicode.com/posts")
-
-  //   const data = await res.json()
-  //       // .then((res) => res.json())
-  //       // .then(
-  //       //   (data) => {
-  //       //     return(data);
-  //       //   }
-  //       //   );
  
-  // }
-
-  const changeState = async () => {
-
   }
+  */
+  const changeScreenFromLogInToSetterMainPage = () => {
+    setSetter(!isSetter)
+  }
+  // function to check input
 
   // states for content container set here
-
-
 
   return ( 
     <Router>
       <GlobalStyle />
-      <form method="GET" onSubmit={changeState}>
-        <input type="text" placeholder="enter 1 or 2">
-
-        </input>
-        <button>Submit</button>
-      </form>
-
+      <button onClick={changeScreenFromLogInToSetterMainPage}>CLICK ME :)</button>
       {/* <TravellersTrollies /> */}
-      <ContentContainer {/* will take in a prop that will be a boolean for whether it's a setter or a completer*/}/>
+      {/* the state is passed through to the container as a prop */}
+      <ContentContainer stateOfContainer={isSetter}/>
     </Router>
   );
 };
--- src/Components/General/ContentContainer/ContentContainerMain.js
+++ src/Components/General/ContentContainer/ContentContainerMain.js
@@ -2,20 +2,24 @@ import React from 'react';
 import OptionsSelector from '../OptionsSelector/OptionsSelector.js';
 import ResponseContainer from '../ResponseContainer/ResponseContainer.js';
 
-
-const ContentContainer = ({/* prop here will take in information*/}) => {
+// this is the prop
+const ContentContainer = ({ stateOfContainer }) => {
     //just to start
     // if prop is one thing, will return data into the container
     // if prop is another thing, will return another set of data into the container
     return (
         <>
-            <h1>This is for { {/* need to get the name of the prop */} ? "SETTERS" : "COMPLETERS" }</h1>
+            {/* h1 will render differently based on what the prop is, whether it's true or false */}
+            <h1>This is for { stateOfContainer ? "SETTERS" : "COMPLETERS" }</h1>
             {/* <OptionsSelector {/* */}
             {/* <ResponseContainer /> */}
         </>
     );
 };
 
+
+ContentContainer.propTypes = {
+}
 
 
 
