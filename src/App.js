ee67ea5d4dd9a0053b1b0f7b4398b3e062a15899
Samy <samyadams11@live.co.uk>
2021-04-21 12:53:17 +0100

    Added modal and temp sticky app bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import { useState } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import TravellersTrollies from './Components/General/AppLayout/AppLayoutMain';
-
+import StickyAppBar from './Components/General/StickyAppBar';
+import LoginPopup from './Components/General/LoginPopup';
 // import ContentContainer from './Components/ContentContainer/ContentContainerMain'
 
 function App() {
+	// states for content container set here
+	const [loginPopupOpen, setLoginPopupOpen] = useState(false);
 
-  // states for content container set here
-  
+	const openLoginPopup = () => {
+		setLoginPopupOpen(true);
+	};
 
+	const closeLoginPopup = () => {
+		setLoginPopupOpen(false);
+	};
 
-  return ( 
-    <Router>
-      <GlobalStyle />
-
-      <TravellersTrollies />
-      {/* <ContentContainer /> */}
-    </Router>
-  );
-};
+	return (
+		<Router>
+			<GlobalStyle />
+			<StickyAppBar openLoginPopup={openLoginPopup} />
+			<TravellersTrollies />
+			{/* <ContentContainer /> */}
+			{/* {popupShown && <LoginPopup />} */}
+			<LoginPopup open={loginPopupOpen} closeLoginPopup={closeLoginPopup} />
+		</Router>
+	);
+}
 
 export default App;
