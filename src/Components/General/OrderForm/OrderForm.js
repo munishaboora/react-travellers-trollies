e0276b5d26c86ab5fe7d3b67af2cddbf5ac13c9f
aselperera <aselperera@hotmail.com>
2021-04-22 18:58:45 +0100

    Can now submit form inputs

--- src/Components/General/OrderForm/OrderForm.js
+++ src/Components/General/OrderForm/OrderForm.js
@@ -1,51 +1,59 @@
-const onSubmit = (e) => {
-	e.preventDefault();
-	// console.log('Hello World!');
-
-	const receipt = fetch(
-		'http://localhost:5000/place_order_and_find_volunteer',
-		{
-			method: 'POST',
-			//prettier-ignore
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({
-				customer_id: '1',
-				product_names: ['Yucca', 'Rhubarb', 'Yucca'],
-			}),
-		}
-	)
-		.then((response) => response.json())
-		.then((data) => console.log(data));
-
-	console.log(receipt);
-
-	// prettier-ignore
-	// const data = await fetch(`http://localhost:5000/${customerOrVolunteer}_login`, {
-	//     method: 'POST',
-	//     headers: {
-	//         'Accept': 'application/json',
-	//         'Content-Type': 'application/json',
-	//     },
-	//     body: JSON.stringify([
-	//         {
-	//             "username": values.username,
-	//             "password": values.password,
-	//         },
-	//     ]),
-	// }).then((response) => response.json())
-};
+import { useState } from 'react';
 
 const OrderForm = () => {
+	const [itemOne, setItemOne] = useState('');
+	const [itemTwo, setItemTwo] = useState('');
+	const [itemThree, setItemThree] = useState('');
+
+	const onSubmit = (e) => {
+		e.preventDefault(); // Needed to prevent default submission action
+
+		console.log(itemOne);
+
+		const receipt = fetch(
+			'http://localhost:5000/place_order_and_find_volunteer',
+			{
+				method: 'POST',
+				//prettier-ignore
+				headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+				body: JSON.stringify({
+					customer_id: '1',
+					product_names: [`${itemOne}`, `${itemTwo}`, `${itemThree}`],
+				}),
+			}
+		)
+			.then((response) => response.json())
+			.then((data) => console.log(data));
+	};
+
 	return (
 		<div>
 			<h1>Basket</h1>
 			<form onSubmit={onSubmit}>
-				<input type='text'></input>
-				<input type='text'></input>
-				<input type='text'></input>
+				<label>Item 1</label>
+				<input
+					type='text'
+					value={itemOne}
+					onChange={(e) => setItemOne(e.target.value)}
+				></input>
+
+				<label>Item 2</label>
+				<input
+					type='text'
+					value={itemTwo}
+					onChange={(e) => setItemTwo(e.target.value)}
+				></input>
+
+				<label>Item 3</label>
+				<input
+					type='text'
+					value={itemThree}
+					onChange={(e) => setItemThree(e.target.value)}
+				></input>
+
 				<button type='submit'>Place Order</button>
 			</form>
 		</div>
