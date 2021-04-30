323245ae308ca2626b7793095309d125d07117eb
harriette <harrietteb@gmail.com>
2021-04-30 20:46:28 +0100

    login route generates json web token if successful

--- ecogain-backend/app.py
+++ ecogain-backend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt 
+import datetime
 
 
 
@@ -30,6 +32,8 @@ def create_user():
 
     return jsonify({'message' : 'new user created'})
 
+
+
 @app.route('/user', methods=['GET'])
 def get_all_users():
     users = User.query.all()
@@ -45,6 +49,8 @@ def get_all_users():
         output.append(user_data)
 
     return jsonify({'users': output})
+
+
 
 @app.route('/user/<user_id>', methods=['GET'])
 def get_one_user(user_id):
@@ -63,6 +69,8 @@ def get_one_user(user_id):
     # might wanna change these returns to not include 'user'
     return jsonify({ 'user': user_data})
 
+
+
 # for if we want a delete profile section on profile page
 @app.route('/user/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
@@ -74,3 +82,34 @@ def delete_user(user_id):
     db.session.delete(user)
     db.session.commit()
     return jsonify ({'message': 'user deleted'})
+
+
+
+#allow to take username and pword
+# use http authentication
+#get a token which will expire after some time
+#use token in header for subsequent req
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        # if no auth info at all / no user/no pwordthen return the following
+        return make_response('could not verify', 401, {'WWW-Authenticate' : 'Basic realm= "Login required!"'})
+    
+    # if there is auth information
+    # want to get the user
+    user = User.query.filter_by(username=auth.username).first()
+
+    if not user:
+        return jsonify({'message': ' no user found'})
+
+    # then the user does exist
+    # need to check pword
+    if check_password_hash(user.password_hash, auth.password):
+        # then generate token
+        token = jwt.encode({'id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+
+    # if pword incorrect
+    return make_response('could not verify', 401, {'WWW-Authenticate': 'Basic realm= "Login required!"'})
