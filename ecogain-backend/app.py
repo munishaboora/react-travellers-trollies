9190998c7dd35b4bccda082268bdb498371abdba
Jaishal Mehta <jaishalmehta@Jaishals-MacBook-Air.local>
2021-04-30 12:07:08 +0100

    Added SQLAlchemy and Migrate changes to file

--- ecogain-backend/app.py
+++ ecogain-backend/app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from flask_sqlalchemcy import SQLAlchemy 
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+db = SQLALCHEMY(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def hello_world():
