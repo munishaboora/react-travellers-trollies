b1b95c3d1a7757e086326706672b3516f12c576b
Jaishal Mehta <jaishalmehta@Jaishals-MacBook-Air.local>
2021-04-30 15:56:08 +0100

    Install Flask-Login and implemented UserMixin within User Class

--- ecogain-backend/models.py
+++ ecogain-backend/models.py
@@ -1,7 +1,8 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'User'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String)
@@ -18,6 +19,10 @@ class User(db.Model):
 
     def check_password(self, password):
        return check_password_hash(self.password_hash, password)
+
+    @login.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
 class Activity(db.Model):
     __tablename__ = 'Activity'
