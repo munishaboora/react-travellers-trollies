b809178f06e258b5c534bb46a35ceac1f14590e5
Jaishal Mehta <jaishalmehta@Jaishals-MacBook-Air.local>
2021-04-30 15:52:31 +0100

    Created Functions for Password Hash

--- ecogain-backend/models.py
+++ ecogain-backend/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'User'
@@ -8,6 +9,15 @@ class User(db.Model):
     email = db.Column(db.String)
     password_hash = db.Column(db.String(128))
     total_points = db.Column(db.Integer)
+
+    def __repr__(self):
+       return '<User {}>'.format(self.username) 
+
+    def set_password(self, password):
+       self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+       return check_password_hash(self.password_hash, password)
 
 class Activity(db.Model):
     __tablename__ = 'Activity'
