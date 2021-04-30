5befa1633bf00d2543acedbc5909d9f201bbf96f
Jaishal Mehta <jaishalmehta@Jaishals-MacBook-Air.local>
2021-04-30 15:26:00 +0100

    Changes to Login

--- ecogain-frontend/src/components/Login/Login.js
+++ ecogain-frontend/src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import './Login.css'
+import './Login.css' 
 // import RegisterPage from './../pages/RegisterPage'
 
 const Login = () => {
